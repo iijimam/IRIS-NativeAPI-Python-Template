fe61ba87eb3d8f44460ee2643b91670b34d69585
iijimam <miijima@intersystems.com>
2021-05-19 02:02:42 +0000

    whlのみ削除するように変更

--- Python/pipinstall-linux.sh
+++ Python/pipinstall-linux.sh
@@ -4,7 +4,7 @@
 pip3 install matplotlib
 pip3 install networkx
 
-rm -rf wheel
+rm -rf wheel/*.whl
 # NativeAPI用whlファイルのGET（Linux用）
 wget -P wheel https://github.com/intersystems/quickstarts-python/raw/master/Solutions/nativeAPI_wheel/irisnative-1.0.0-cp34-abi3-linux_x86_64.whl
 pip3 install wheel/irisnative-1.0.0-cp34-abi3-linux_x86_64.whl
--- Python/pipinstall-mac.sh
+++ Python/pipinstall-mac.sh
@@ -3,7 +3,7 @@
 pip3 install matplotlib
 pip3 install networkx
 
-rm -rf wheel
+rm -rf wheel/*.whl
 
 wget -P wheel https://github.com/intersystems/quickstarts-python/raw/master/Solutions/nativeAPI_wheel/irisnative-1.0.0-cp34-abi3-macosx_10_13_x86_64.macosx_10_14_x86_64.whl
 pip3 install wheel/irisnative-1.0.0-cp34-abi3-macosx_10_13_x86_64.macosx_10_14_x86_64.whl
