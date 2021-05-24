d4be66ec47526bcf77cabe962e5910db205b4ae7
iijimam <miijima@intersystems.com>
2021-05-24 09:05:59 +0000

    ディレクトリの記述ミス

--- Python/pipinstall.bat
+++ Python/pipinstall.bat
@@ -2,7 +2,7 @@
 rm -rf wheel
 
 bitsadmin /transfer myDownloadJob /download /priority normal https://github.com/iijimam/IRISModules/raw/master/python/wheel/intersystems_irispython-3.2.0-py3-none-any.whl %CD%/wheel/intersystems_irispython-3.2.0-py3-none-any.whl
-pip install wheel/intersystems_irispython-3.2.0-py3-none-any.whl
+pip install ./wheel/intersystems_irispython-3.2.0-py3-none-any.whl
 
 pip install matplotlib
 pip install networkx
